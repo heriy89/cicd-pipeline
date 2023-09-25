824822d2c0ee64030920cbaab062e5ac853c4973
S@nt089 <herisusanto89@gmail.com>
2023-09-25 15:49:41 +0700

    add features

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 )
 
-func main() {
+func main()) {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
+	http.listening()
 }
